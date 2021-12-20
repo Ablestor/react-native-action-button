ab53a456a44ce81f81649e6810e37e90f4abc05e
TAEWOONG KANG <ktw2378@korea.ac.kr>
2021-12-20 14:14:29 +0900

    class에서 hooks로 변경 (해결방법 왜 class로 나와있는데;)

--- ActionButton.js
+++ ActionButton.js
@@ -255,35 +255,35 @@ const ActionButton = (props) => {
   //////////////////////
 
   const animateButton = (animate = true) => {
-    if (this.state.active) return this.reset();
+    if (active) return reset(animate);
 
     if (animate) {
-      Animated.spring(this.anim, {
+      Animated.spring(anim.current, {
         toValue: 1,
         useNativeDriver: false,
       }).start();
     } else {
-      this.anim.setValue(1);
+      anim.current.setValue(1);
     }
 
-    this.setState({ active: true, resetToken: this.state.resetToken });
+    setActive(true);
   };
 
   const reset = (animate = true) => {
-    if (this.props.onReset) this.props.onReset();
+    if (props.onReset) props.onReset();
 
     if (animate) {
-      Animated.spring(this.anim, {
+      Animated.spring(anim.current, {
         toValue: 0,
         useNativeDriver: false,
       }).start();
     } else {
-      this.anim.setValue(0);
+      anim.current.setValue(0);
     }
 
-    setTimeout(() => {
-      if (this.mounted) {
-        this.setState({ active: false, resetToken: this.state.resetToken });
+    timeout.current = setTimeout(() => {
+      if (mounted.current) {
+        setActive(false);
       }
     }, 250);
   };
